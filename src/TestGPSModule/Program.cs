47258e6ea461fec7ae0737c6928bf0beca613cb8
Mif Masterz <mifmasterz@yahoo.com>
2020-05-19 15:01:22 +0700

    init

---
+++ src/TestGPSModule/Program.cs
@@ -0,0 +1,19 @@
+﻿using GHIElectronics.TinyCLR.Data.GpsParser;
+using GHIElectronics.TinyCLR.Pins;
+using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace TestGPSModule
+{
+    class Program
+    {
+        static void Main()
+        {
+            GpsDevice device = new GpsDevice(SC20260.UartPort.Uart5);
+            device.StartGPS();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
